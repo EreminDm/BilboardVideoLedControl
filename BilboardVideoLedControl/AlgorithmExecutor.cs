317e6e3fe9a31f02d1016ec8dc1193428ce16930
Дмитрий <eremin.dm@gmail.com>
2017-05-10 18:05:47 +0600

    add module class

---
+++ BilboardVideoLedControl/AlgorithmExecutor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilboardVideoLedControl
+{
+    class AlgorithmExecutor
+    {
+        public void executeAlgorihtms(CheckAlgorithm[] algo)
+        {
+            foreach(CheckAlgorithm alg in algo){
+                alg.execute();
+            }
+        }
+    }
+}
--- BilboardVideoLedControl/Program.cs
+++ BilboardVideoLedControl/Program.cs
@@ -6,10 +6,123 @@ using System.Threading.Tasks;
 
 namespace BilboardVideoLedControl
 {
-    class Program
+    class LedControl
     {
-        static void Main(string[] args)
+        public void InitLedman(string[] args)
         {
+            /*
+            ** Sender initialization
+            */
+            try
+            {
+                Console.WriteLine("Start!");
+                Console.WriteLine("Processing...");
+                if (NativeMethods.ReConnectSender())
+                {
+                    Console.WriteLine("Is connected");
+                }
+                else
+                {
+                    Console.WriteLine("Reconnect is false;");
+                }
+            }
+            catch (Exception e9)
+            {
+                Console.WriteLine(e9.Message);
+            }
+            Console.ReadKey();
+            /*
+             ** Reciver initialization
+             */
+
+            /*
+             ** Modules initialization
+             */
+
+        }
+        
+        public ushort getSenders()
+        {
+            return NativeMethods.GetConnectSenderCount();
+        }
+
+        public int getReceivers()
+        {
+            return NativeMethods.GetFoundReceiverCount();
+        }
+
+
+    }
+
+    interface CheckAlgorithm
+    {
+        Boolean execute();
+        String getResult();
+        void setLedControl(LedControl control);
+    }
+
+    class SenderCheck : CheckAlgorithm
+    {
+        private LedControl control;
+        public bool execute()
+        {
+            Boolean result = false;
+            int senders = (int)control.getSenders();
+            for(int step=0; step < senders; step++)
+            {
+                NativeMethods.GetSenderId((byte)step);
+            }
+            throw new NotImplementedException();
+        }
+
+        public string getResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setLedControl(LedControl control)
+        {
+            this.control = control;
         }
     }
+
+
+    class ReceiverCheck : CheckAlgorithm
+    {
+        public bool execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setLedControl(LedControl control)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class ModuleCheck : CheckAlgorithm
+    {
+        private LedControl control;
+        public bool execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setLedControl(LedControl control)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
 }
