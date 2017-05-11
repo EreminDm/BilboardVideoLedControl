2f320420c0e50f0b2274ad295209809850a2f289
Дмитрий <eremin.dm@gmail.com>
2017-05-11 17:05:49 +0600

    add receiver and module check algoritms

--- BilboardVideoLedControl/Program.cs
+++ BilboardVideoLedControl/Program.cs
@@ -51,6 +51,10 @@ namespace BilboardVideoLedControl
             return NativeMethods.GetFoundReceiverCount();
         }
 
+        /*public int getModules()
+        {
+            
+        }*/
 
     }
 
@@ -80,9 +84,10 @@ namespace BilboardVideoLedControl
             for(int step=0; step < senders; step++)
             {
                 int id = NativeMethods.GetSenderId((byte)step);
+                bool inputSignail = NativeMethods.GetSenderDviInput((byte)step, out ushort rHorResolution, out ushort rVerResolution); // What is are resolutions?
                 if (true)
                 {
-                    result = buildJsonResult(id,false,true);
+                    result = buildJsonResult(id, false, inputSignail);
                     res = true;
                 }
             }
@@ -103,9 +108,34 @@ namespace BilboardVideoLedControl
 
     class ReceiverCheck : CheckAlgorithm
     {
+        private LedControl control;
+        private String result;
+
+        private String buildJsonResult(int recId, bool recInfo, bool recBadPanelsInfo)
+        {
+            return "{\"receiverd\":\"" + recId.ToString() + "\",\"outSignal\":\"" + recInfo.ToString() + "\",\"inSignal\":\"" + recBadPanelsInfo.ToString() + "\"";
+        }
         public bool execute()
         {
-            throw new NotImplementedException();
+            Boolean res = false;
+            int reseivers = (int)control.getReceivers();
+            
+            NativeMethods.tagReceiverIdInfo recIdInfo;
+            NativeMethods.tagReceiverBadPanels recBadPanels;
+            
+            for (int step = 0; step < reseivers; step++)
+            {
+                int recId = step;
+                bool recInfo = NativeMethods.GetReceiverIdInfo(out recIdInfo, (ushort)step);
+                bool recBadPanelsInfo = NativeMethods.GetReceiverBadPanels((ushort)step, out recBadPanels);
+                if (true)
+                {
+                    result = buildJsonResult(recId, recInfo, recBadPanelsInfo);
+                    res = true;
+                }
+            }
+
+                return res;
         }
 
         public string getResult()
@@ -121,10 +151,29 @@ namespace BilboardVideoLedControl
 
     class ModuleCheck : CheckAlgorithm
     {
+        private String buildJsonResult(bool moduleFullInfo)
+        {
+            return "{\"receiverd\":\"" + moduleFullInfo.ToString() + "\"";
+        }
+
         private LedControl control;
+        private String result;
         public bool execute()
         {
-            throw new NotImplementedException();
+            Boolean res = false;
+            int reseivers = (int)control.getReceivers();
+            NativeMethods.tagReceiverModuleDetailInfo moduleInfo;
+
+            for (int step = 0; step < reseivers; step++)
+            {
+                bool moduleFullInfo = NativeMethods.QueryReceiverModuleDetailInfo(out moduleInfo, (ushort)step);
+                if (true)
+                {
+                    result = buildJsonResult(moduleFullInfo);
+                    res = true;
+                }
+            }
+            return res;
         }
 
         public string getResult()
